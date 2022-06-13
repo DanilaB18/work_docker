2294815c8b9e78e0d3a583aaf8336696977ffd3a
DanilaB18 <106802352+DanilaB18@users.noreply.github.com>
2022-06-13 10:32:25 +0300

    Update ApplicationController.java

--- src/main/java/com/eiswebtest/controller/ApplicationController.java
+++ src/main/java/com/eiswebtest/controller/ApplicationController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping_fake;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
  
 @Controller
